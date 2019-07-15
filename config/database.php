ed51fd5ae0e3e66df4cd700f3fbedaa9ca22f60b
edringtondc <edringtonDC@gmail.com>
2019-07-15 13:08:47 -0600

    added mySql to config for heroku deployment

--- config/database.php
+++ config/database.php
@@ -2,6 +2,13 @@
 
 use Illuminate\Support\Str;
 
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+$host = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$database = substr($url["path"], 1);
+
 return [
 
     /*
@@ -34,6 +41,17 @@ return [
     */
 
     'connections' => [
+        
+        'your_heroku_mysql_connection' => array(
+    'driver' => 'mysql',
+    'host' => $host,
+    'database' => $database,
+    'username' => $username,
+    'password' => $password,
+    'charset' => 'utf8',
+    'collation' => 'utf8_unicode_ci',
+    'prefix' => '',
+),
 
         'sqlite' => [
             'driver' => 'sqlite',
