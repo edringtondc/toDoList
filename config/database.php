35563349e2771eb4cfdca06d876f499a573f62bb
Elisabeth Edrington <edringtondc@gmail.com>
2019-07-15 18:52:26 -0600

    add heroku creds

--- config/database.php
+++ config/database.php
@@ -48,8 +48,6 @@ return [
     'database' => $database,
     'username' => $username,
     'password' => $password,
-    'charset' => 'utf8',
-    'collation' => 'utf8_unicode_ci',
     'prefix' => '',
 ),
 
